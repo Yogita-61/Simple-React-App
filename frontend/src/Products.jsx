ecf773a6682086f77e762f9751f918b089dc9e8d
Yogita-61 <yogita.r2024aiml@sece.ac.in>
2025-12-19 09:19:58 +0530

    Update API endpoint for fetching products

--- frontend/src/Products.jsx
+++ frontend/src/Products.jsx
@@ -25,7 +25,7 @@ export default function Products({ addToCart }) {
       }
 
       try {
-        const res = await fetch("http://localhost:5000/api/products", {
+        const res = await fetch("http://56.228.2.255:6116/api/products", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
